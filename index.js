4234257ac5faeb43f0dfb26e9ea8abd67241b609
Nikita <nikitaloktev71@gmail.com>
2023-07-28 19:07:07 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,51 @@
+import express from "express";
+import fs from "fs";
+import mongoose from "mongoose";
+
+import {
+  registerValidation,
+  loginValidation,
+  postCreateValidation,
+} from "./validations.js";
+
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
+
+import { UserController, PostController } from "./controllers/index.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://admin:loktev@cluster0.3rlj0er.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
+
+const app = express();
+
+app.use(express.json());
+app.post(
+  "/auth/login",
+  loginValidation,
+  handleValidationErrors,
+  UserController.login
+);
+app.post(
+  "/auth/register",
+  registerValidation,
+  handleValidationErrors,
+  UserController.register
+);
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.get("/posts", PostController.getAll);
+// app.get("/posts/:id", PostController.getOne);
+app.post("/posts", checkAuth, postCreateValidation, PostController.create);
+// app.delete("/posts", UserController.remove);
+// app.patch("/posts", UserController.update);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log("Server OK");
+});
